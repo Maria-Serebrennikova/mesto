c08eab35d73543e550db24e80dfab15732ffc13d
Maria Serebrennikova <maria.silver2015@yandex.ru>
2022-01-23 13:16:18 +0300

    fix: исправлены ошибки после первой проверки

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,21 @@ const openPopupButton = document.querySelector(".profile__rename");
 const popupContainer =  document.querySelector(".popup");
 const closePopupButton = document.querySelector(".popup__close");
 
-const nameInput = document.querySelector(".popup__name");
-const jobInput = document.querySelector(".popup__status");
+const nameInput = document.querySelector(".popup__info.popup__info_type_name");
+const jobInput = document.querySelector(".popup__info.popup__info_type_status");
 
-const formElement = document.querySelector(".popup__form");
-
+const formElement = document.querySelector('.popup__form[name="profile-form"]');
 const profileName = document.querySelector(".profile__name");
 const profileStatus = document.querySelector(".profile__status");
 
 function openPopup() {
-    popupContainer.classList.add("popup_type_active");
+    popupContainer.classList.add("popup_active");
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileStatus.textContent;
 }
 
 function closePopup() {
-    popupContainer.classList.remove("popup_type_active");
+    popupContainer.classList.remove("popup_active");
 
     nameInput.value = "";
     jobInput.value = "";
@@ -29,9 +30,16 @@ function formSubmitHandler(e) {
 
     const name = nameInput.value;
     const job = jobInput.value;
-    
+ if (name.trim()) {
     profileName.textContent = name;
+
+ }
+
+ if (job.trim()) {
     profileStatus.textContent = job;
+
+ }
+    
 
     closePopup();
 }
